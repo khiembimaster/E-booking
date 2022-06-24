d3dff999e4897457a8ea739dcb1fd8850a458c2c
khiembimaster <masterkhiembi@gmail.com>
2022-06-25 00:24:28 +0700

    try somthing new

--- Client.py
+++ Client.py
@@ -6,17 +6,29 @@ import tkinter
 def Main_menu():
     """Login or Register"""
     msg = client_socket.recv(BUFSIZE).decode("utf8")
-    label_greeting_from_server = tkinter.Label(master=frame_start, text = msg)
+    label_greeting.destroy()
+    label_greeting_from_server = tkinter.Label(master=frame_main_menu, text = msg)
     label_greeting_from_server.pack()
-    frame_login.pack()
-    frame_register.pack()
+    frame_start.pack_forget()
+    frame_main_menu.pack()
+    
 
 def Login():
     """Login"""
+    frame_main_menu.pack_forget()
+    client_socket.send(bytes("Logg","utf8"))
+    msg = (client_socket.recv(BUFSIZE)).decode("utf8")
+    print(msg)
+    return
+    # log_info = {}
+    # log_info["name"] = input()
+    # log_info["password"] = input()
+    
 
 def Register():
     """Register"""
-
+    frame_main_menu.destroy() 
+    client_socket.send(bytes("Resgister","utf8"))
 
 #Inter-face
 window = tkinter.Tk()
@@ -24,14 +36,13 @@ window.title("E-booking")
 frame_start = tkinter.Frame()
 label_greeting = tkinter.Label(master=frame_start, text = "Please connect to server...")
 label_greeting.pack()
-frame_login = tkinter.Frame()
-button_login = tkinter.Button(master=frame_login, text = "Login", command=Login)
-button_login.pack()
-frame_register = tkinter.Frame()
-button_register = tkinter.Button(master=frame_register, text = "Register", command=Register)
-button_register.pack()
-
 frame_start.pack()
+
+frame_main_menu = tkinter.Frame()
+button_login = tkinter.Button(master=frame_main_menu, text = "Login", command=Login)
+button_login.pack()
+button_register = tkinter.Button(master=frame_main_menu, text = "Register", command=Register)
+button_register.pack()
 
 
 
